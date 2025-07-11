f7a34da8c66137903f9b3b2f9ceba9b1df32582b
Aminur Rahman <dev.aminur@gmail.com>
2025-07-11 12:28:00 +0600

    Try to fix ReactCurrentOwner error

--- client/src/app/admin/app-users/[key]/page.tsx
+++ client/src/app/admin/app-users/[key]/page.tsx
@@ -12,18 +12,7 @@ export default async function UserDetailsPage({
     <div className="flex min-h-screen w-full flex-col">
       <div className="flex flex-col">
         <main className="flex-1 px-4 sm:px-6 lg:px-8">
-          <Suspense
-            fallback={
-              <div className="flex items-center justify-center min-h-[400px]">
-                <div className="flex items-center space-x-2">
-                  <Loader2 className="h-6 w-6 animate-spin" />
-                  <span>Loading user details...</span>
-                </div>
-              </div>
-            }
-          >
-            <AppKeyDetailsPage appKey={key} />
-          </Suspense>
+          <AppKeyDetailsPage appKey={key} />
         </main>
       </div>
     </div>
