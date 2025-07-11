15a4347ab0845dc1dce91f0bdfd75c7319272b2f
Aminur Rahman <dev.aminur@gmail.com>
2025-07-11 12:25:02 +0600

    Try to attempt fix error

--- client/src/app/admin/app-users/[key]/page.tsx
+++ client/src/app/admin/app-users/[key]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import AppKeyDetailsPage from "./details";
+import { Loader2 } from "lucide-react";
 
 export default async function UserDetailsPage({
   params,
@@ -11,7 +12,16 @@ export default async function UserDetailsPage({
     <div className="flex min-h-screen w-full flex-col">
       <div className="flex flex-col">
         <main className="flex-1 px-4 sm:px-6 lg:px-8">
-          <Suspense fallback={<div>Loading ...</div>}>
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center min-h-[400px]">
+                <div className="flex items-center space-x-2">
+                  <Loader2 className="h-6 w-6 animate-spin" />
+                  <span>Loading user details...</span>
+                </div>
+              </div>
+            }
+          >
             <AppKeyDetailsPage appKey={key} />
           </Suspense>
         </main>
